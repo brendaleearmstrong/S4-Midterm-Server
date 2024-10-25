30aec10863bf10808a59e50ad837ca0a235ed0d1
Ethan Murphy <ethanmurphy267@gmail.com>
2024-10-25 20:48:39 -0230

    Added spring web

---
+++ src/main/java/com/misight/server/MisightApplication.java
@@ -0,0 +1,13 @@
+package com.misight.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misight/server/MisightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misight.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
