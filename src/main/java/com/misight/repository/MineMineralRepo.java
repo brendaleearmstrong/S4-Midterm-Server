4bcd6118919686998583958a23ec1a8b055867a8
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-11-05 10:13:09 +0100

    Delete Model: MineMinerals

--- src/main/java/com/misight/repository/MineMineralRepo.java
+++
@@ -1,9 +0,0 @@
-package com.misight.repository;
-
-import com.misight.model.MineMineral;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MineMineralRepo extends JpaRepository<MineMineral, Long> {
-}
